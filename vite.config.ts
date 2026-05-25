cff5a8cced5871e3ce88dde002a53f2f0b3e26c4
dddin12 <halabaahhh@gmail.com>
2026-05-25 16:10:59 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/hpgrdcrcfl/",
+  base: "/hpgrdcrcfl",
   server: {
     host: "::",
     port: 8080,
